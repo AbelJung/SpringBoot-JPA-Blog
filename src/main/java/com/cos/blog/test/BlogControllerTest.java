492088d227abfc25c11b444f3c52dc63bc4b7b39
AbelJung <junghb1209@naver.com>
2021-02-23 09:04:48 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,22 @@
+package com.cos.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+				//컨트롤러
+				// 스프링이 com.cos.blog 패키지 이하를 스캔
+			    // 모든 파일을 메모리에 new하는 것은 아님.
+				// 특정 @어노테이션이 붙어있는 클래스 파일들을 new해서 (=IOC) 스프링 컨테이너에서 관리해줌
+
+@RestController
+public class BlogControllerTest {
+	
+	//http://localhost:8787/test/hello
+	@GetMapping("/test/hello")
+	public String hello() {
+		
+		return "<h1>hello spring boot </h1>";
+	}
+	
+}
